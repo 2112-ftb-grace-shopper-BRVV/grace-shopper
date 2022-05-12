7ceaef16cac25be4b3893038365156b85c01abc1
root <root@DESKTOP-RAP30DD>
2022-05-11 19:34:35 -0500

    worked on db users and added to index and changed name of url

--- db/models/user.js
+++ db/models/user.js
@@ -1,11 +1,118 @@
 // grab our db client connection to use with our adapters
 const client = require('../client');
+const bcrypt = require("bcrypt")
+
+async function createUser({ username, password, address, email }) {
+  const SALT_COUNT = 10;
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+  try {
+    const {
+      rows: [user]
+    } = await client.query(
+      `
+        INSERT INTO users(username, password, address, email) 
+        VALUES($1, $2, $3, $4) 
+        ON CONFLICT (username) DO NOTHING 
+        RETURNING *;
+      `,
+      [username, hashedPassword, address, email]
+    );
+    delete user.password;
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateUser(id, fields = {}){
+
+  const setString = Object.keys(fields).map(
+      (key, index) => `"${ key }"=$${ index + 1}`
+  ).join(', ');
+
+  if (setString.length === 0) {
+      return;
+  }
+
+  try {
+      const { rows: [ user ] } = await client.query(`
+      UPDATE users
+      SET ${ setString }
+      WHERE id=${ id }
+      RETURNING *;
+      `, Object.values(fields));
+
+      return user;
+  } catch (error){
+      throw error;
+  }
+}
+
+async function getAllUsers() {
+  try {
+    const { rows } = await client.query(`
+        SELECT id, username, address, email
+        FROM users;
+    `);
+
+    return rows;
+}   catch (error) {
+    throw error;
+}
+}
+
+async function getUserById(id) {
+  try {
+    const { rows: [ user ] } = await client.query(`
+      SELECT id, username, address, email
+      FROM users
+      WHERE id=$1
+    `, [id]);
+
+    if (!user) {
+      return null
+    }
+
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user]  } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE username=$1
+    `, [username]);
+
+    if (!user) {
+      return null
+    }
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
 
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
+  createUser,
+  updateUser,
+  getUserByUsername,
+  getUserById
 };
 
-async function getAllUsers() {
-  /* this adapter should fetch a list of users from your db */
-}
+
+const { client,
+  getAllUsers,
+  createUser,
+  updateUser,
+  getUserById
+  
+} = require('./db/user');
