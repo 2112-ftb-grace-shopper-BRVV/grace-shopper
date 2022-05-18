d174cf6f429ca271702f660e2124c80b4fd48d0e
vinnyzef <72477005+vinnyzef@users.noreply.github.com>
2022-05-18 18:10:28 -0500

    fixed sum stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useDebugValue, useEffect, useState } from "react"
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 // getAPIHealth is defined in our axios-services directory index.js
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
 import { getAPIHealth } from '../axios-services';
 import '../style/App.css';
+import Products from './Products';
+import Login from "./logIn";
 
 const App = () => {
   const [APIHealth, setAPIHealth] = useState('');
+
+  console.log("Hello!")
 
   useEffect(() => {
     // follow this pattern inside your useEffect calls:
@@ -22,11 +32,22 @@ const App = () => {
     getAPIStatus();
   }, []);
 
+
   return (
+
+<Router>
     <div className="app-container">
-      <h1>Hello, World!</h1>
-      <p>API Status: {APIHealth}</p>
+        <h1>Hello, World!</h1>
+        <p>API Status: {APIHealth}</p>
+    <Switch>   
+      
+      <Products/>
+    
+    
+    </Switch>
+
     </div>
+    </Router>
   );
 };
 
