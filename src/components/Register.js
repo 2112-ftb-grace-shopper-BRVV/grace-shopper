fa0c6e614aa92bc04493813cc121bca5d81f110d
Ryan <rsull12@lsu.edu>
2022-05-21 12:17:50 -0500

    for vinny to pull down

--- src/components/Register.js
+++ src/components/Register.js
@@ -20,7 +20,7 @@ const Register = (props) => {
     
     try{
 
-    const response = await fetch("http://localhost:4000/api/user/register", {
+    const response = await fetch("http://localhost:3000/register", {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
