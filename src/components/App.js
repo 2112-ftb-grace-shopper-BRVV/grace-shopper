0b61bef0a01c63f2b7833b0ce4946c4f9cd7ffd7
vinnyzef <72477005+vinnyzef@users.noreply.github.com>
2022-06-01 14:21:47 -0500

    working full product and singular product view with minor issues to be resolved

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useDebugValue, useEffect, useState } from "react"
-
+import WebFont from 'webfontloader';
 import {
     BrowserRouter as Router,
     Switch,
@@ -14,10 +14,18 @@ import Products from './Products';
 import Login from "./logIn";
 
 import HotSauces from './HotSauces'
-import BBQsauces from "./BBQsauces";
+import SingleProduct from "./singleProduct";
+
 
 
 const App = () => {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: [ 'Chivo', 'sans-serif']
+      }
+    });
+   }, []);
   const [APIHealth, setAPIHealth] = useState('');
 
   //holds state of token to be used for login and logout check
@@ -53,18 +61,17 @@ const App = () => {
   return (
 
 <Router>
-    <div className="app-container">
-        <h1>SAUCE SPOT</h1>
+    <div  style={ {color: "grey", fontFamily: ['Chivo', 'sans-serif']}}className="app-container">
+        <h1 style={{color:"black", letterSpacing:"-2px"}}>
+          SAUCE  SP<img style= {{  height: "25px", width: "30px"}}src="https://cdn-icons-png.flaticon.com/128/3/3835.png"/>T
+          </h1>
         <p>API Status: {APIHealth}</p>
+        < Route exact path= "/products/singleProduct"><SingleProduct/></Route>
+        <Route exact path= "/products"><Products/></Route>
+        <Route path= "/products/hotshop"><HotSauces/></Route>
         <Route><Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/></Route>
-    <Switch>   
 
-      {/* {token? null :(<Route><Login/></Route>)} */}
-      <Route exact path= "/products"><Products/></Route>
-      <Route path= "/products/hotshop"><HotSauces/></Route>
-      <Route path= "/products/smokeshop"><BBQsauces/></Route>
 
-    </Switch>
 
     </div>
     </Router>
