70087999bbbcdf0990779d1fb4e36fc21d148828
root <root@DESKTOP-RAP30DD>
2022-06-04 15:16:38 -0500

    completed styling on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,25 @@ import Login from './logIn';
 const Navbar =(props)=>{
     const {isLoggedIn, setIsLoggedIn} = props
     
-        return (<nav style={{ display: "flex", width: "100%", height: "10vw", alignItems: "center", justifyContent: "flex-start", background: "linear-gradient(to bottom, lightgrey, white", }}>
-        <Link to="/" style={{ textDecoration: "none", color: "grey" }}>
-          <h1 style={{ color: "black", letterSpacing: "-2px", marginRight: "50vw" }}> SAUCE  SP<img style={{ height: "20px", width: "50px" }} src="https://cdn-icons-png.flaticon.com/128/3/3835.png" />T</h1></Link>
-        <Route><Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} /></Route>
-        <Link to="/products/hotshop" style={{ textDecoration: "none", color: "black", marginRight: "2vw" }}>SAUCES</Link>
-        <Link style={{ textDecoration: "none", color: "black" }} to="/cart">CART</Link>
-        <Link to="/Register" style={{ textDecoration: "none", color: "black", marginRight: "2vw", marginLeft: "2vw" }}>REGISTER</Link>
+        return (
+        <nav style={{ display: "flex", width: "100%", height: "10vw", alignItems: "center", background: "linear-gradient(to bottom, grey, white" }}>
+
+          <Link to="/" style={{ textDecoration: "none", color: "grey" }}>
+            <h1 style={{ display:"flex",justifyContent:"flex-start",alignContent:"center", flexDirection:"row", color: "black", letterSpacing: "1px",marginLeft:"5vw" }}> SAUCE SP<img style={{ height: "25px", width: "30px" }} src="https://cdn-icons-png.flaticon.com/128/3/3835.png" />T</h1>
+          </Link>
+
+          <Route>
+            <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          </Route>
+
+          <Link to="/products/hotshop" className='navtab' >SAUCES
+          </Link>
+
+          <Link className='navtab' to="/cart">CART
+          </Link>
+
+          <Link to="/Register" className='navtab' >SIGN UP
+          </Link>
       </nav>
       
     )}
